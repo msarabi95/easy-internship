652506e8ad7b9c344c275cc88fe98c4ccfa9cac2
Muhammad Saeed Arabi <msarabi95@gmail.com>
2016-06-30 17:50:36 +0300

    - Added `Internship` and `Rotation` to the admin site

--- planner/admin.py
+++ planner/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from planner.models import Hospital, Department, Specialty, Rotation, Internship
 
-# Register your models here.
+
+class RotationInline(admin.TabularInline):
+    model = Rotation
+    extra = 0
+
+
+class InternshipAdmin(admin.ModelAdmin):
+    inlines = [RotationInline]
+
+admin.site.register(Hospital)
+admin.site.register(Specialty)
+admin.site.register(Department)
+admin.site.register(Internship, InternshipAdmin)
