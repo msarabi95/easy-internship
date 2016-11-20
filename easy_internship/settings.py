e7a9f1787b5aadd58526d886d5b5d4564d736cdd
Muhammad Saeed Arabi <msarabi95@gmail.com>
2016-11-20 19:53:39 +0300

    Moved all static files into a new folder separate from Django apps. This includes:   - the angular app js files   - the angular partials   - all css and js libraries (including AdminLTE theme)   - any other static files or images  User bower to manage front-end packages, packages are not included in the repo anymore  Update requirements.txt  Remove unused travis.yml

--- easy_internship/settings.py
+++ easy_internship/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend')
+]
 STATIC_URL = '/static/'
 STATIC_ROOT = secrets.STATIC_ROOT
 
