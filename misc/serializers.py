f278cfdd2bdd08f84710f51ede3aabdc95467e22
Zaha Jamaan <zahajamaan@gmail.com>
2017-02-04 20:50:47 +0300

    feture announcment update

--- misc/serializers.py
+++ misc/serializers.py
@@ -8,20 +8,20 @@ class AnnouncementSerializer(serializers.ModelSerializer):
 
 
     def create(self,validated_data):
-        if validated_data.is_published:
-            instance.publish_datetime = timezone.now()
-            instance.save()
-        return instance
+        if not validated_data.is_published:
+            validated_data.publish_datetime = None
+            validated_data.save()
+        return alidated_data
 
     def update(self, instance, validated_data):
         if validated_data.is_published and not instance.is_published:
-            instance.publish_datetime = timezone.now()
-            instance.save()
-            return instance
+            validated_data.publish_datetime = timezone.now()
+            validated_data.save()
+            return validated_data
         elif not validated_data.is_published:
-            instance.publish_datetime = None
-            instance.save()
-        return instance
+            validated_data.publish_datetime = None
+            validated_data.save()
+            return validated_data
 
     class Meta:
         model = Announcement
