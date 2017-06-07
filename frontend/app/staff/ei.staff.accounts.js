cd0354922faec1c86bc955addba9cea0d8cd7bc6
Muhammad Saeed Arabi <msarabi95@gmail.com>
2017-06-08 02:10:44 +0300

    - Popover to show remaining months for plans that require more viewing space

--- frontend/app/staff/ei.staff.accounts.js
+++ frontend/app/staff/ei.staff.accounts.js
@@ -13,7 +13,7 @@ angular.module("ei.staff.accounts", ["ei.months.models", "ei.accounts.models",
             controller: "InternListCtrl"
         })
         .when("/interns/summary/", {
-            templateUrl: "static/partials/staff/interns/plans-summary.html?v=0002",
+            templateUrl: "static/partials/staff/interns/plans-summary.html?v=0003",
             controller: "PlansSummaryCtrl"
         })
         .when("/interns/:id/", {
