bae4457989106ad9047b181fdcf05f6fd2930c7e
Muhammad Saeed Arabi <msarabi95@gmail.com>
2017-06-08 16:48:37 +0300

    Update intern planner page to make use of faster loading and animations

--- frontend/app/intern/ei.intern.months.js
+++ frontend/app/intern/ei.intern.months.js
@@ -9,7 +9,7 @@ angular.module("ei.months", ["ei.hospitals.models", "ei.months.models", "ei.rota
 
     $routeProvider
         .when("/planner/", {
-            templateUrl: "static/partials/intern/months/month-list.html?v=0005",
+            templateUrl: "static/partials/intern/months/month-list.html?v=0006",
             controller: "MonthListCtrl"
         })
         .when("/planner/:month_id/", {
@@ -37,91 +37,10 @@ angular.module("ei.months", ["ei.hospitals.models", "ei.months.models", "ei.rota
 
 }])
 
-.controller("MonthListCtrl", ["$scope", "loadWithRelated", "Intern", "InternshipMonth", "Rotation", "RotationRequest", "RequestedDepartment", "Department", "Hospital", "Specialty", "LeaveType", "Leave", "LeaveRequest", "LeaveCancelRequest",
-    function ($scope, loadWithRelated, Intern, InternshipMonth, Rotation, RotationRequest, RequestedDepartment, Department, Hospital, Specialty, LeaveType, Leave, LeaveRequest, LeaveCancelRequest) {
-        $scope.moment = moment;
-
-        Intern.query(function (interns) {
-            $scope.intern = interns[0];
-        });
-        $scope.months = InternshipMonth.query();
-
-        $scope.months.$promise.then(function (results) {
-
-            var occupied = [];
-            var requested = [];
-
-            // Save the indices of occupied and requested months in 2 separate arrays
-            // Also add 2 flags (occupied and requested) with the appropriate boolean values to each month object
-            angular.forEach(results, function (month, index) {
-                if (month.current_rotation !== null) {
-                    occupied.push(index);
-                    $scope.months[index].occupied = true;
-                } else {
-                    $scope.months[index].occupied = false;
-                }
-
-                if (month.current_rotation_request !== null) {
-                    requested.push(index);
-                    $scope.months[index].requested = true;
-                } else {
-                    $scope.months[index].requested = false;
-                }
-            });
-
-            // Load all details of occupied months
-            angular.forEach(occupied, function (monthIndex) {
-                // Load current rotation
-                $scope.months[monthIndex].current_rotation = loadWithRelated($scope.months[monthIndex].current_rotation, Rotation, [
-                    [{department: Department}, [
-                        {specialty: Specialty},
-                        {hospital: Hospital}
-                    ]]
-                ]);
-                // Load current leaves, leave requests, and leave cancel requests
-                $scope.months[monthIndex].current_leaves = loadWithRelated($scope.months[monthIndex].current_leaves, Leave, [{type: LeaveType}]);
-                $scope.months[monthIndex].current_leave_requests = loadWithRelated($scope.months[monthIndex].current_leave_requests, LeaveRequest, [{type: LeaveType}]);
-                $scope.months[monthIndex].current_leave_cancel_requests = loadWithRelated($scope.months[monthIndex].current_leave_cancel_requests, LeaveCancelRequest);
-            });
-
-            // Load all details of requested month
-            angular.forEach(requested, function (monthIndex) {
-                $scope.months[monthIndex].current_rotation_request = loadWithRelated($scope.months[monthIndex].current_rotation_request, RotationRequest, [
-                    {specialty: Specialty},
-                    [{requested_department: RequestedDepartment}, [
-                        [{department: Department}, [
-                            {hospital: Hospital}
-                        ]]
-                    ]]
-                ]);
-            })
-
-        });
-
-        $scope.getTileClass = function (month) {
-            if (!month.disabled && !month.frozen) {
-                if (!month.occupied && !month.requested) {
-                    if (!month.current_freeze_request) {
-                        return "default";
-                    } else {
-                        return "warning";
-                    }
-                } else if (!month.occupied && month.requested) {
-                    return "warning";
-                } else if (month.occupied && !month.requested) {
-                    return "primary";
-                //} else if (month.occupied && month.requested && month.current_rotation_request.delete) {
-                //    return "danger";
-                } else {
-                    return "primary";
-                }
-            } else if (!!month.frozen) {
-                return "info";
-            } else {
-                return "default";
-            }
-
-        };
+.controller("MonthListCtrl", ["$scope", "Internship", function ($scope, Internship) {
+    $scope.internship = Internship.query(function (internships) {
+        $scope.internship = internships[0];
+    });
 }])
 
 .controller("MonthDetailCtrl", ["$scope", "$location", "$routeParams", "loadWithRelated", "InternshipMonth", "Hospital", "Department", "Specialty", "Rotation", "RotationRequest", "RequestedDepartment", "RotationRequestResponse", "RotationRequestForward", "LeaveType", "Leave", "LeaveRequest", "LeaveRequestResponse", "LeaveCancelRequest",
