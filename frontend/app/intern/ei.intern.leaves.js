c9bef307e20a68b64e0c25181d01e124a1b9ee1a
Muhammad Saeed Arabi <msarabi95@gmail.com>
2017-06-15 01:06:02 +0300

    - Leave request angular view now properly submits and shows error messages

--- frontend/app/intern/ei.intern.leaves.js
+++ frontend/app/intern/ei.intern.leaves.js
@@ -1,14 +1,14 @@
 /**
  * Created by MSArabi on 11/17/16.
  */
-angular.module("ei.leaves", ["ngRoute", "djng.forms", "ui.select", "ei.utils",
+angular.module("ei.leaves", ["ngRoute", "ngFileUpload", "ui.select", "ei.utils", "ui.bootstrap", "ei.accounts.models",
                              "ei.hospitals.models", "ei.months.models", "ei.rotations.models", "ei.leaves.models"])
 
 .config(["$routeProvider", function ($routeProvider) {
 
     $routeProvider
         .when("/planner/:month_id/request-leave/", {
-            templateUrl: "static/partials/intern/leaves/request-leave.html",
+            templateUrl: "static/partials/intern/leaves/request-leave.html?v=0001",
             controller: "LeaveRequestCreateCtrl"
         })
         .when("/planner/:month_id/leaves/history/", {
@@ -22,13 +22,20 @@ angular.module("ei.leaves", ["ngRoute", "djng.forms", "ui.select", "ei.utils",
 
 }])
 
-.controller("LeaveRequestCreateCtrl", ["$scope", "$http", "$routeParams", "$location", "djangoForm", "InternshipMonth", "LeaveType",
-    function ($scope, $http, $routeParams, $location, djangoForm, InternshipMonth, LeaveType) {
+.controller("LeaveRequestCreateCtrl", ["$scope", "$routeParams", "Upload", "$location", "User", "LeaveType", "LeaveSetting",
+    function ($scope, $routeParams, Upload, $location, User, LeaveType, LeaveSetting) {
+        User.query(function (users) {
+            $scope.intern = users[0];
+        });
+
         $scope.month = moment({
             year: Math.floor($routeParams.month_id / 12),
             month: ($routeParams.month_id % 12)
         });
         $scope.leaveTypes = LeaveType.query();
+        $scope.leaveSettings = LeaveSetting.query();
+
+        $scope.leave_request = {};
 
         $scope.startDateOptions = $scope.endDateOptions = {
             minDate: $scope.month.toDate(),
@@ -38,47 +45,44 @@ angular.module("ei.leaves", ["ngRoute", "djng.forms", "ui.select", "ei.utils",
             showWeeks: false
         };
 
-        $scope.$watch('leaveRequestData.start_date_as_date', function (newValue, oldValue) {
-            if (newValue !== undefined) {
-                $scope.leaveRequestData.start_date = moment(newValue).format('MM/DD/YYYY');
-                $scope.endDateOptions.minDate = moment(newValue).toDate();
-            }
-        });
-
-        $scope.$watch('leaveRequestData.end_date_as_date', function (newValue, oldValue) {
-            if (newValue !== undefined) {
-                $scope.leaveRequestData.end_date = moment(newValue).format('MM/DD/YYYY');
-                $scope.startDateOptions.maxDate = moment(newValue).toDate();
-            }
-        });
-
-        $scope.$watch('leaveRequestForm.$message', function (newValue, oldValue) {
-            if (newValue !== undefined && newValue !== "") {
-                toastr.warning(newValue);
-                $scope.leaveRequestForm.$message = undefined;
-            }
-        });
-
         $scope.submit = function () {
-            if ($scope.leaveRequestData) {
 
-                $scope.leaveRequestData.month = $routeParams.month_id;
+            var data = $scope.leave_request;
+            data.intern = $scope.intern.id;
+            data.month = $scope.month.year() * 12 + ($scope.month.month());
+            data.start_date = moment(data.start_date).format('YYYY-MM-DD');
+            data.end_date = moment(data.end_date).format('YYYY-MM-DD');
 
-                $scope.submission = $http.post(
-                    "/leaves/leave-request-form/",
-                    $scope.leaveRequestData
-                );
-                $scope.submission.success(function (out_data) {
-                    if (!djangoForm.setErrors($scope.leaveRequestForm, out_data.errors)) {
-                        $location.path("/planner/" + $scope.month.month);
+            // Submit
+            $scope.upload = Upload.upload({
+                url: '/api/leave_requests/',
+                data: data,
+                method: "POST"
+            });
+            $scope.upload.then(function (resp) {
+                $location.path('/planner');
+
+            }, function (resp) {
+                if (resp.status !== 400) {
+                    console.log(resp);
+                    toastr.error(resp);
+                } else {
+                    $scope.leave_request_form.$message = resp.data.non_field_errors;
+                    const fields = ['type', 'start_date', 'end_date'];
+                    for (var i in fields) {
+                        var item = fields[i];
+                        if (!$scope.leave_request_form[item]) {
+                            continue;
+                        }
+
+                        $scope.leave_request_form[item].$message = resp.data[item];
+                        if (!!$scope.leave_request_form[item].$message) {
+                            $scope.leave_request_form[item].$setPristine(true);
+                        }
                     }
-                }).error(function (error) {
-                    console.log(error);
-                    toastr.error(error);
-                })
-            }
+                }
+            });
 
-            return false;
         };
 
 }])
