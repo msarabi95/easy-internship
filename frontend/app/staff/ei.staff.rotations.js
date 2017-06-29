b000f5d4105711245f82fbd3c32ec5382987cb68
Muhammad Saeed Arabi <msarabi95@gmail.com>
2017-06-29 19:59:14 +0300

    Updated styling of monthly list tables

--- frontend/app/staff/ei.staff.rotations.js
+++ frontend/app/staff/ei.staff.rotations.js
@@ -22,7 +22,7 @@ angular.module("ei.staff.rotations", ["ei.hospitals.models", "ei.months.models",
             controller: "MasterRotaCtrl"
         })
         .when("/rotations/:department/:month_id/", {
-            templateUrl: "static/partials/staff/rotations/monthly-list.html?rel=1498746955401",
+            templateUrl: "static/partials/staff/rotations/monthly-list.html?rel=1498756221883",
             controller: "MonthlyListCtrl"
         });
 
@@ -322,38 +322,13 @@ angular.module("ei.staff.rotations", ["ei.hospitals.models", "ei.months.models",
         };
 }])
 
-.controller("MonthlyListCtrl", ["$scope", "$routeParams", "DTOptionsBuilder", "DTColumnBuilder", "Batch", "Department", "Rotation", function ($scope, $routeParams, DTOptionsBuilder, DTColumnBuilder, Batch, Department, Rotation) {
+.controller("MonthlyListCtrl", ["$scope", "$routeParams", "Batch", "Department", function ($scope, $routeParams, Batch, Department) {
     $scope.month = moment({year: Math.floor(parseInt($routeParams.month_id)/ 12), month: (parseInt($routeParams.month_id) % 12)});
     $scope.department = Department.get({id: $routeParams.department});
 
-    $scope.dtOptions = {};
-
     $scope.batches = Batch.query(function (batches) {
-
         angular.forEach(batches, function (batch) {
-            $scope.dtOptions['batch_' + String(batch.id)] = DTOptionsBuilder
-                .fromFnPromise(function() {
-                    return Batch.monthly_list({id: batch.id, department: $routeParams.department, month: $routeParams.month_id}).$promise;
-                })
-                .withOption("order", [[ 1, "asc" ]])
-                .withOption("responsive", true)
-                .withBootstrap();
+            batch.monthly_list = Batch.monthly_list({id: batch.id, department: $routeParams.department, month: $routeParams.month_id});
         });
     });
-
-    $scope.dtColumns = [
-        DTColumnBuilder.newColumn(null).withTitle(null).notSortable()
-            .renderWith(function (data, type, full, meta) {
-                return '<img src="' + data.internship.intern.profile.mugshot + '" class="img-circle img-bordered-sm img-sm"/>';
-            }),
-        DTColumnBuilder.newColumn('internship.intern.profile.en_full_name').withTitle('Name'),
-        DTColumnBuilder.newColumn('internship.intern.student_number').withTitle('Student Number'),
-        DTColumnBuilder.newColumn('internship.intern.badge_number').withTitle('Badge Number'),
-        //DTColumnBuilder.newColumn('internship.intern.profile.user.email').withTitle('Email'),
-        DTColumnBuilder.newColumn('internship.intern.mobile_number').withTitle('Mobile Number'),
-        DTColumnBuilder.newColumn(null).withTitle(null).notSortable()
-            .renderWith(function (data, type, full, meta) {
-                return '<a class="btn btn-default btn-flat" href="#/interns/' + data.internship.id + '/">View details</a>';
-            })
-    ];
 }]);
